11d02a17f5acda068674c1fc07eb60adb3191055
q695933585 <695933585@qq.com>
2018-08-06 06:26:43 +0000

    Updated database configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+
+require __DIR__.'/helpers.php';
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -10,7 +13,6 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-require __DIR__.'/helpers.php';
 
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
